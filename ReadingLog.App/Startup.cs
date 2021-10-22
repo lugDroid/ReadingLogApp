e8bf2dffaa6c46a814e33bab1a307a385c4b9e33
lugDroid <diazbastos@gmail.com>
2021-10-22 15:20:03 +0200

    Delete template pages and set book list as landing page

--- ReadingLog.App/Startup.cs
+++ ReadingLog.App/Startup.cs
@@ -33,6 +33,10 @@ namespace ReadingLog.App
             });
 
             services.AddScoped<IReadingLogRepository, SqliteRepository>();
+
+            services.AddMvc().AddRazorPagesOptions(options => {
+                options.Conventions.AddPageRoute("/Books/List", "");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
