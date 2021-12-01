eda641bd781b674550f582262cd80334b8b95d55
lugDroid <diazbastos@gmail.com>
2021-12-01 11:20:57 +0100

    Remove custom login/logout pages

--- ReadingLog.App/Startup.cs
+++ ReadingLog.App/Startup.cs
@@ -22,7 +22,8 @@ namespace ReadingLog.App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages().AddMvcOptions(o => o.Filters.Add(new AuthorizeFilter()));
+            //services.AddRazorPages().AddMvcOptions(o => o.Filters.Add(new AuthorizeFilter()));
+            services.AddRazorPages();
 
             services.AddDbContextPool<ReadingLogDbContext>(options => {
                 options.UseSqlite(Configuration.GetConnectionString("SqliteContext"));
