7d86281c71ef8a8f2b4c5dcf8ec3c074860057c5
lugDroid <diazbastos@gmail.com>
2021-12-01 07:03:38 +0100

    Scaffold identity with default UI

---
+++ ReadingLog.App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ReadingLog.App.Areas.Identity.Data;
+
+[assembly: HostingStartup(typeof(ReadingLog.App.Areas.Identity.IdentityHostingStartup))]
+namespace ReadingLog.App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityDataContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityDataContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityDataContext>();
+            });
+        }
+    }
+}
