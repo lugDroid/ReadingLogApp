e12b1de5dc3f7b62615ed688a4f94cc798eb020e
lugDroid <diazbastos@gmail.com>
2021-12-10 11:43:43 +0100

    Add user secrets to store Google Books api key

--- ReadingLog.App/Pages/Authors/Detail.cshtml.cs
+++ ReadingLog.App/Pages/Authors/Detail.cshtml.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using ReadingLog.Data;
 
 namespace ReadingLog.App.Pages.Authors
@@ -6,16 +8,20 @@ namespace ReadingLog.App.Pages.Authors
     public class DetailModel : PageModel
     {
         private readonly IAuthorRepository authorRepository;
+        private readonly IConfiguration config;
 
         public Author Author { get; set; }
 
-        public DetailModel(IAuthorRepository authorRepository)
+        public DetailModel(IAuthorRepository authorRepository, IConfiguration config)
         {
             this.authorRepository = authorRepository;
+            this.config = config;
         }
         public void OnGet(int authorId)
         {
             Author = authorRepository.GetAuthorById(authorId);
+
+            var apiKey = config["GoogleBooksApiKey"];
         }
     }
 }
